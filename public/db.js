bfb6b7ed31cb9d3fa780ad792c41c389deab9a38
forbegos <forbegos@me.com>
2021-06-17 21:02:07 -0600

    created file db.js

---
+++ public/db.js
@@ -0,0 +1,55 @@
+let db;
+let budgetVersion;
+
+const request = indexedDB.open("budgetDB", budgetVersion || 21);
+
+request.onupgradeneeded = function (event) {
+  const { oldVersion } = event;
+  const newVersion = event.newVersion || db.version;
+  db = event.target.result;
+
+  if (db.objectStoreNames.lenght === 0) {
+    db.createObjectStore("BudgetStore", { autoincrement: true });
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+function checkDatabase() {
+  var transaction = db.transaction(["BudgetStore"], "readwrite");
+  const txs = transaction.objectStore("BudgetStore");
+  const getAll = txs.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          if (res.length !== 0) {
+            transaction = db.transaction(["BudgetStore"], "readwrite");
+            const tsx = transaction.objectStore("BudgetStore");
+            tsx.clear();
+          }
+        });
+    }
+  };
+}
+
+window.addEventListener("online", checkDatabase);
